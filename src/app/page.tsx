c192ca53e299ba340b3e83efeaf993b154b92fb6
lemkhayeryassine2002 <lemkhayeryassine2002@gmail.com>
2023-12-11 21:41:57 +0100

    eighth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <h1 className='text-5xl sm:text-6xl mb-2 dark:text-[#DADADA]'>I&#39;m a Front End Web Developer</h1>
           <p className='text-lg dark:text-[#DADADA]'>Let me Code for You</p>
           <Link href={`/projects`}>
-            <button type="button" className="mt-8 mb-2 sm:mb-0 mr-4 text-black bg-white border-[1px] border-[#dadada] hover:bg-[#dadada] font-medium rounded-lg text-base px-5 py-2.5">Projects</button>
+            <button type="button" className="mt-8 mb-3 sm:mb-0 mr-4 text-black bg-white border-[1px] border-[#dadada] hover:bg-[#dadada] font-medium rounded-lg text-base px-5 py-2.5">Projects</button>
           </Link>
           <button type="button" className="bg-black border-[1px] border-spgray hover:bg-[#222] text-white font-medium rounded-lg text-base px-5 py-2.5">Resume</button>
         </div>
